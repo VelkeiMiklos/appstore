2f26a5a89d4975241725544610baddf2c5f40ddb
VelkeiMiklos <velkei18@gmail.com>
2018-02-20 16:50:56 +0100

    Initial Commit

---
+++ appstore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appstore
+//
+//  Created by Velkei Miklós on 2018. 02. 20..
+//  Copyright © 2018. NeonatCore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
